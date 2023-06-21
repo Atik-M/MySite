c9fd27ed7f1f7469c958666cb222c8f9702e1391
Uday Patel <uday@initialyze.com>
2023-06-21 14:32:44 +0530

    updated the consumertype for the card model

--- core/src/main/java/com/mysite/core/models/Card.java
+++ core/src/main/java/com/mysite/core/models/Card.java
@@ -1,6 +1,9 @@
 package com.mysite.core.models;
 
+import org.osgi.annotation.versioning.ConsumerType;
 
+
+@ConsumerType
 public interface Card extends com.adobe.cq.wcm.core.components.models.Teaser {
     
 
